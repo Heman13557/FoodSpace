e90f4edf64f9d664600ff4dc87e62cd56064942a
heman13557 <hkahariyana@gmail.com>
2021-11-23 16:31:40 +0530

    New Commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require("dotenv/config")
+
+mongoose.connect(process.env.DB_cred,{ useNewUrlParser : true},()=>{
+    console.log("connected")
+})
 
 const app = express();
 
+
+app.use(express.json())
+
 app.get("/", (req,res)=>{
     res.send("Server Working!");
+
 });
 
 app.listen(4000, ()=>{
