614b079052b376c8a7210513887dabc45d74e71f
heman13557 <hkahariyana@gmail.com>
2021-12-13 15:17:13 +0530

    Added Res.json

--- index.js
+++ index.js
@@ -14,47 +14,36 @@ app.use(express.urlencoded({extended: true}));
 
 mongoose.connect(process.env.DB_cred,{ useNewUrlParser: true });
 
-const newMenu = new Menu({
-    menuName: "Special",
-    dishes: {
-        dishName: "Roasted veggies",
-        description: "Serves 8-10",
-        calories: 400,
-        dishImage: "https://i1.wp.com/www.nourishedtheblog.com/wp-content/uploads/2016/03/Balsamic-Honey-Roasted-Vegetables-1.jpg?resize=1543,2160&ssl=1"
+app.get("/api/menu", (req,res)=>{
+    try{
+        console.log(Menu.find({},(err,result)=>{
+            if(result.length){
+            res.json(result)
+        }
+        else console.log("Error")
+    }));
+}
+    catch(e){
+        console.log(e.message);
     }
-});
-
-const newReview = new Review({
-    R_Name : "Hemant",
-    R_view : "Very Delicious"
-});
-
-
-app.get("/", (req,res)=>{
-    Menu.find({},(err,result)=>{
-        if(result.length === 0){
-            Menu.insertMany(newMenu, (err)=>{
-                if(err) console.log(err);
-                else    console.log("Inserted Successfully");
-            });
-        }
-        else if(result.length){
-            console.log(result)
-        }
     });
-    Review.find({},(err,result)=>{
-        if(result.length === 0){
-            Review.insertMany(newReview,(err)=>{
-        if(err) console.log(err);
-        else console.log("Review Submitted");
+
+    app.get("/api/reviews", (req,res)=>{
+    try{
+        Review.find({},(err,result)=>{
+            if(result.length){
+            res.json(result)
+        }
+        else console.log("Error")
     });
 }
-else if(result.length){
-    console.log(result)
-}
-});
+    catch(e){
+        console.log(e.message);
+    }
+    });
+
+app.get("/",(req,res)=>{
     res.send("Created DB!");
-
 });
 
 async function submit_review(obj){
